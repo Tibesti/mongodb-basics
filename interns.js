ba33f81aa340f7a8abe09ad817078a111604d384
Tibesti <51659129+Tibesti@users.noreply.github.com>
2020-04-13 19:39:31 -0600

    Update and rename collection.js to interns.js

---
+++ interns.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+const insertDocuments = function(db,callback){
+  const collection = db.collection('myMovies');
+  collection.insertMany(
+    [
+      {
+        movie:"The Banker",
+        year:2020,
+        rating:8
+      },
+      {
+        movie:"Bad Boys",
+        year:2020,
+        rating:7
+      },
+      {
+        movie:"The Hunt",
+        year:2020,
+        rating:7
+      },
+      {
+        movie:"Bloodshot",
+        year:2020,
+        rating:7.5
+      },
+      {
+        movie:"First Cow",
+        year:2020,
+        rating:6.5
+      }
+    ], function(err, result){
+        assert.equal(err,null);
+        assert.equal(5, result.result.n);
+        assert.equal(5, result.ops.length);
+        console.log("Inserted 5 documents into the collection");
+        callback(result);
+    }
+  );
+};
+module.exports = insertDocuments;
