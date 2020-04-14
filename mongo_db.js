5184821a913314c7db939a74dff41d0937468d95
Tibesti <51659129+Tibesti@users.noreply.github.com>
2020-04-13 19:37:37 -0600

    Update mongo_db.js

--- mongo_db.js
+++ mongo_db.js
@@ -1,7 +1,26 @@
 var MongoClient = require ('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/tibesti";
-MongoClient.connect(url, function(err, db){
+
+const insertDocuments = require("./interns");
+const findDocuments = require("./findInterns")
+const updateDocuments = require("./updateInterns")
+
+const dbName = "tibesti";
+const client = new MongoClient (url,{ useUnifiedTopology: true, useNewUrlParser: true });
+client.connect(function(err){
     if (err) throw err;
-    console.log('Database created by Tibesti!');
-    db.close();
+    const db = client.db(dbName);
+    console.log("Database created by Tibesti");
+    insertDocuments(db,function(result){
+        console.log(result);
+        client.close
+    });
+    findDocuments(db, function(){
+        console.log(result);
+        client.close;
+    })
+    updateDocuments(db, function(){
+        console.log(result);
+        client.close;
+    })
 });
